aedb3e73b96586df27d0d680e5041caa255f76e8
gelena <helenatcitok@gmail.com>
2024-05-16 09:42:25 +0100

    HW_26.2(3)

--- config/settings.py
+++ config/settings.py
@@ -182,7 +182,7 @@ CELERY_TASK_TIME_LIMIT = 30 * 60
 
 CELERY_BEAT_SCHEDULE = {
     "task-name": {
-        "task": "users.tasks",  # Путь к задаче
+        "task": "users.tasks.check_user_activity",  # Путь к задаче
         "schedule": timedelta(
             minutes=10
         ),  # Расписание выполнения задачи (например, каждые 10 минут)
